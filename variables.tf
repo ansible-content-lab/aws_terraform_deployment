aceb6c977f5252ca9f7f6e0944f78e01811cb047
Leena Jawale <ljawale@redhat.com>
2023-11-28 14:52:57 -0500

    validation for deployment_id
    Signed-off-by: Leena Jawale <ljawale@redhat.com>

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
 variable "deployment_id" {
   description = "Creates a random string that will be used in tagging for correlating the resources used with a deployment of AAP."
   type    = string
+  validation {
+    condition     = length(var.deployment_id) == 8
+    error_message = "deployment_id length should be 8 chars"
+  }
 }
