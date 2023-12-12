431459c43d6e97375c8650eb7e9282453daef78c
Jonathan Holloway <loadtheaccumulator@gmail.com>
2023-12-12 16:05:48 -0600

    add terraform RDS DB module (#6)
    Signed-off-by: Jonathan Holloway <jholloway@redhat.com>

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -11,3 +11,8 @@ output "infrastructure_igw" {
   description = "Internet gateway ID"
   value = aws_internet_gateway.aap_infrastructure_igw.id
 }
+
+output "infrastructure_sg_id" {
+  description = "Security group name"
+  value = aws_security_group.aap_infrastructure_sg.id
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,70 @@ variable "deployment_id" {
     error_message = "deployment_id length should be 8 chars and should contain lower case alphabets only"
   }
 }
+
+# Database variables
+variable "infrastructure_db_allocated_storage" {
+  description = "The allocated storage in gibibytes"
+  type = number
+  default = 100
+}
+
+variable "infrastructure_db_allow_major_version_upgrade" {
+  description = "Indicates that major version upgrades are allowed"
+  type = bool
+  default = false
+}
+
+variable "infrastructure_db_auto_minor_version_upgrade" {
+  description = "Indicates that minor engine upgrades will be applied automatically to the DB instance"
+  type = bool
+  default = true
+}
+variable "infrastructure_db_instance_class" {
+  description = "The instance type of the RDS instance"
+  type = string
+  default = "db.m5d.xlarge"
+}
+
+variable "infrastructure_db_engine_version" {
+  description = "The database engine version to use"
+  type = string
+  default = "13.12"
+}
+
+variable "infrastructure_db_multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ"
+  type = bool
+  default = false
+}
+
+variable "infrastructure_db_storage_encrypted" {
+  description = "Specifies whether the DB instance is encrypted"
+  type = bool
+  default = true
+}
+
+variable "infrastructure_db_storage_iops" {
+  description = "The amount of provisioned IOPS"
+  type = number
+  default = 5000
+}
+
+variable "infrastructure_db_storage_type" {
+  description = "The type of storage to use (defaults to io1 if iops is defined)"
+  type = string
+  default = "io1"
+}
+
+variable "infrastructure_db_username" {
+  description = "Database instance username"
+  type = string
+  default = "ansible"
+}
+
+variable "infrastructure_db_password" {
+  description = "Database instance password"
+  type = string
+  sensitive = true
+  default = "changeme"
+}
