cef3f3a20ea027cfb624a71ac2075827fdad1ba5
sharvarikhedkar <shkhedka@redhat.com>
2023-12-12 03:27:06 -0800

    delete redundant output file

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "deployment_id" {
-  description = "Print Deployment ID"
-  value = var.deployment_id == "" ? random_string.deployment_id[0].id : var.deployment_id
-}
-
-output "vpc_module_outputs" {
-  description = "VPC outputs"
-  value = module.vpc
-}
