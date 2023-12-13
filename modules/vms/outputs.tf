90da406ac6968dbb9f201657b362772177821247
sharvarikhedkar <shkhedka@redhat.com>
2023-12-13 11:05:50 -0800

    output instance public and private ip

--- modules/vms/outputs.tf
+++ modules/vms/outputs.tf
@@ -2,3 +2,13 @@ output "instance_id" {
   description = "Instance ID"
   value = aws_instance.aapvm.id
 }
+
+output "vm_public_ip" {
+  description = "Public IP address assigned to the instance"
+  value = aws_instance.aapvm.public_ip
+}
+
+output "vm_private_ip" {
+  description = "Private IP address assigned to the instance"
+  value = aws_instance.aapvm.private_ip
+}
